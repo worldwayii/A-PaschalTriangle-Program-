a61e8cbfc2ba4a1468dfbc1886f6bd8956a4b0fb
chucks <eluichuma@gmail.com>
2016-05-20 13:31:30 +0100

    A new commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaschalTriangle
+{
+    class PaschalTriangle
+    {
+        public static void Main( string [] args)
+        {
+            const int HEIGHT = 20;
+
+            long[][] triangle = new long[HEIGHT + 1][];
+
+            for (int row = 0; row < HEIGHT; row++)
+            {
+                triangle[row] = new long[row + 1];
+            }
+
+            triangle[0][0] = 1;
+            for (int row = 0; row < HEIGHT - 1; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    triangle[row + 1][col] += triangle[row][col];
+                    triangle[row + 1][col + 1] += triangle[row][col];
+                }
+            }
+            for (int row = 0; row < HEIGHT; row++)
+            {
+                Console.Write("".PadLeft((HEIGHT - row) * 2));
+                for (int col = 0; col <= row; col++)
+                {
+                    Console.Write("{0,3}", triangle[row][col]);
+                }
+                    Console.WriteLine(); 
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PaschalTriangle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PaschalTriangle")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("31f76427-6662-48a4-a496-967b088d038c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
